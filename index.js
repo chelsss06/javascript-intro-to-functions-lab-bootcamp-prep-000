5f85c8e035e69d0cd14970e75811da4083901244
chelss093@gmail.com <chelss093@gmail.com>
2018-08-18 17:43:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase
+
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase
+
+  var mixedCase = 'Hi there!'
+  mixedCase.toLowerCase() === mixedCase
+
+  mixedCase.toUpperCase() === mixedCase
+  
+  if (string === lowercase) {
+    console.log("I can't hear you!")
+  } else if (string === uppercase) {
+    console.log("YES INDEED!")
+  } else {
+    return("I love you, too.")
+  }
+}
+
