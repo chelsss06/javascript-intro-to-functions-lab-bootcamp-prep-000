1d435e4a235c9916faee51109cf3fa6e4aa59e6d
chelss093@gmail.com <chelss093@gmail.com>
2018-08-18 17:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  var uppercase = "HELLO!"
+  var uppercase = 'HELLO!'
   uppercase.toUpperCase() === uppercase
 
   var lowercase = 'hello!'
